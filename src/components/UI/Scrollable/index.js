b7136da8913abbe58f12217294c7306878598925
M. Çağlar TUFAN <caglartufan09@gmail.com>
2023-08-25 03:15:37 +0300

    Up

--- src/components/UI/Scrollable/index.js
+++ src/components/UI/Scrollable/index.js
@@ -1,11 +1,10 @@
-import { useCallback, useRef } from 'react';
+import './style.css';
 
 const Scrollable = props => {
     const {
         className: customClassName,
         children
     } = props;
-    const scrollableRef = useRef();
 
     let className = 'scrollable-inner bg-card';
 
@@ -13,32 +12,9 @@ const Scrollable = props => {
         className = `${className} ${customClassName}`;
     }
 
-    const mouseOverHandler = useCallback(() => {
-        // const classList = scrollableRef.current.classList;
-
-        // if(classList.contains('scrollable--out')) {
-        //     scrollableRef.current.classList.remove('scrollable--out');
-        // }
-
-        // scrollableRef.current.classList.add('scrollable--over');
-    }, []);
-
-    const mouseOutHandler = useCallback(() => {
-        // const classList = scrollableRef.current.classList;
-
-        // if(classList.contains('scrollable--over')) {
-        //     scrollableRef.current.classList.remove('scrollable--over');
-        // }
-
-        // scrollableRef.current.classList.add('scrollable--out');
-    }, []);
-
     return (
         <div
             className="scrollable"
-            onMouseOver={mouseOverHandler}
-            onMouseOut={mouseOutHandler}
-            ref={scrollableRef}
         >
             <div className={className}>
                 {children}
